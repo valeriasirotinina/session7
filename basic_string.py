d10b6480f116e2ef4bb1678343ddb44d17d8acb7
valeriasirotinina <06valerias@gmail.com>
2026-01-30 11:48:14 +0100

    first string example

---
+++ basic_string.py
@@ -0,0 +1,11 @@
+text = "Hello World"
+print(text)
+text = 'Hello World 2'
+print(text[4])
+print(len(text))
+text = ""
+print(len(text))
+text = "Bob"
+print(text[-1])
+print(text[-2])
+print(text[-3])
